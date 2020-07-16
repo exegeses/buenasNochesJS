5a68791aafe125a4edde0b36c54a0327fdf4139e
marcos pinardi <exegeses@gmail.com>
2020-07-16 19:53:39 -0300

    escribir usando innerText

--- clase2/js/escribir.js
+++ clase2/js/escribir.js
@@ -1,3 +1,5 @@
 // write()
     document.write('Escribir en el HTML usando javascript');
 
+// ubicar un elemento dentro del DOM
+    document.getElementById('parrafo').innerText = 'Texto generado usando innerText';
