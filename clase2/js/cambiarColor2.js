e461c28dba1a187fb2d15f0ccd063ccfce980ceb
marcos pinardi <exegeses@gmail.com>
2020-07-16 22:04:13 -0300

    tarea mostarr/acultar

---
+++ clase2/js/cambiarColor2.js
@@ -0,0 +1,19 @@
+// ubicar elemento/s dentro del DOM
+    var caja = document.getElementById('caja');
+
+// generar funcion/es
+    function verdeAr()
+    {
+        caja.style.backgroundColor = '#81bc93';
+    }
+
+    function rojeAr()
+    {
+        caja.style.backgroundColor = '#dd6161';
+    }
+
+    function celesteAr()
+    {
+        caja.style.backgroundColor = '#62c3c6';
+    }
+
---
+++ clase2/js/mostarrOcultar.js
@@ -0,0 +1,11 @@
+// ubicar elemento/s dentro del DOM
+    var caja = document.getElementById('caja');
+
+// generar función/es
+    function ocultar()
+    {
+        // visibility: hidden|visible
+        // display:  none|block
+        // opacity:  0|1
+        caja.style.visibility = 'hidden';
+    }
