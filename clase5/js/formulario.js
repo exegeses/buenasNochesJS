0dc0c6c0fc02322a1fba0a1ec5bc2e3789f4241e
marcos pinardi <exegeses@gmail.com>
2020-08-06 21:25:38 -0300

    validacion de error en el form

--- clase5/js/formulario.js
+++ clase5/js/formulario.js
@@ -2,6 +2,7 @@
     var txtNombre = document.querySelector('input[name="nombre"]');
     var errorNombre = document.querySelector('#errorNombre');
     var txtEmail = document.querySelector('input[name="email"]');
+    var errorEmail = document.querySelector('#errorEmail');
 //generar funciones
 
     function validar() {
@@ -9,6 +10,13 @@
         if( txtNombre.value == '' ){
             //mostrar mensaje
             errorNombre.innerHTML = '<img src="imagenes/error.png"> Complete el campo Nombre';
+            //evitamos que se envíe el formulario
+            return false;
+        }
+        //si el campo email está vacío
+        if( txtEmail.value == '' ){
+            //mostrar mensaje
+            errorEmail.innerHTML = '<img src="imagenes/error.png"> Complete el campo Email';
             //evitamos que se envíe el formulario
             return false;
         }
